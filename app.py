ebe52eecad4ca83164ae0a9199422ef61d521871
santyago0 <santyago0515@gmail.com>
2025-04-16 08:56:53 -0500

    paso 15 cambio local

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     actual = datetime.now()
     fecha_formateada = actual.strftime("%d, %B, %Y, %M, %H, %S")
-    return f'¡Hola, mundo! <b>{fecha_formateada}</b>'
+    return f'¡Hola, Loja! <b>{fecha_formateada}</b>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
